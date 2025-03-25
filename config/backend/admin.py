d1313cd577633b28149e4a78b38a01342c5aa149
kajitatsu <tatsukajijp@gmail.com>
2025-03-26 04:36:36 +0900

    アドミン・モデルの編集

--- config/backend/admin.py
+++ config/backend/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Restaurant, CommercialFacility
 
 # Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "name", "address", "latitude", "longitude", "access_info", 
+        "commercial_facility", "nursing_room", "diaper_changing_room", 
+        "stroller", "baby_chair", "baby_food", "baby_food_allowed", 
+        "parking", "sunken_kotatsu", "kids_menu", "kids_discount", 
+        "toys_given", "allergy_friendly"
+    )  # 管理画面で表示するフィールド
+    search_fields = ("name", "address")  # 検索可能なフィールド
+
+
+@admin.register(CommercialFacility)
+class CommercialFacilityAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "name", "address", "latitude", "longitude", "access_info", 
+        "nursing_room", "diaper_changing_room", "stroller", "baby_chair", 
+        "parking", "kids_discount"
+    )  # 管理画面で表示するフィールド
+    search_fields = ("name", "address")  # 検索可能なフィールド
--- config/backend/models.py
+++ config/backend/models.py
@@ -19,7 +19,7 @@ class Restaurant(models.Model):
     stroller = models.BooleanField(default=False, verbose_name="ベビーカー")
     baby_chair = models.BooleanField(default=False, verbose_name="ベビーチェア")
     baby_food = models.BooleanField(default=False, verbose_name="離乳食")
-    baby_food_allowed = models.BooleanField(default=False, verbose_name="離乳食持ち込み可否")
+    baby_food_allowed = models.BooleanField(default=False, verbose_name="離乳食持ち込み可")
     parking = models.BooleanField(default=False, verbose_name="駐車場")
     sunken_kotatsu = models.BooleanField(default=False, verbose_name="掘り炬燵")
     kids_menu = models.BooleanField(default=False, verbose_name="お子様ランチ")
@@ -27,8 +27,13 @@ class Restaurant(models.Model):
     toys_given = models.BooleanField(default=False, verbose_name="おもちゃがもらえる")
     allergy_friendly = models.BooleanField(default=False, verbose_name="アレルギー対応可")
 
+    class Meta:
+        verbose_name = "レストラン情報"
+        verbose_name_plural = "レストラン情報"
+
     def __str__(self):
         return self.name
+
 
 class CommercialFacility(models.Model):
     id = models.AutoField(primary_key=True, verbose_name="ID")
@@ -43,6 +48,10 @@ class CommercialFacility(models.Model):
     baby_chair = models.BooleanField(default=False, verbose_name="ベビーチェア")
     parking = models.BooleanField(default=False, verbose_name="駐車場")
     kids_discount = models.BooleanField(default=False, verbose_name="お子様割引有無")
+
+    class Meta:
+        verbose_name = "商業施設情報"
+        verbose_name_plural = "商業施設情報"
 
     def __str__(self):
         return self.name
