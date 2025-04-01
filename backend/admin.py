7c3afafaf5adeaebe8516e1ae4dfb3de573874ab
kajitatsu <tatsukajijp@gmail.com>
2025-04-01 09:13:47 +0900

    レストランモデル修正

--- backend/admin.py
+++ backend/admin.py
@@ -8,8 +8,10 @@ class RestaurantAdmin(admin.ModelAdmin):
         "id", "name", "address", "retty_id", "latitude", "longitude", "access_info", 
         "commercial_facility", "nursing_room", "diaper_changing_room", 
         "stroller", "baby_chair", "baby_food", "baby_food_allowed", 
-        "parking", "sunken_kotatsu", "kids_menu", "kids_discount", 
-        "toys_given", "allergy_friendly"
+        "parking", "horigotatsu", "kids_menu", "kids_discount", 
+        "toys_given", "allergy_friendly", "protein_rich", "vegetable_like",
+        "vegetable_rich", "infants_allowed", "preschoolers_allowed",
+        "elementary_schoolers_allowed", "play_zone", "daycare_facility"
     )  # 管理画面で表示するフィールド
     search_fields = ("name", "address")  # 検索可能なフィールド
 
